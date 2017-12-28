199d5fe34bf9f629ec08bbab4e22bb0e78ffb8de
MagicMarvMan <marvin@marvnet.de>
2017-12-28 09:36:31 +0100

    added basic app-view-model

--- js/app.js
+++ js/app.js
@@ -13,3 +13,18 @@ var defaultLocations = [
 	}
 
 ];
+
+
+
+
+
+
+function AppViewModel() {
+	var self = this;
+
+	this.locations = ko.observableArray([]);
+	map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: {lat: 000, lng: 000}
+	});
+}
