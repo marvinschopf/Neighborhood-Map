6f2259f1127d1884d3ffd558b3e85f6d5f283b25
MagicMarvMan <marvin@marvnet.de>
2017-12-28 16:21:45 +0100

    Removed last change

--- js/app.js
+++ js/app.js
@@ -91,14 +91,12 @@ var Location = function(data) {
 
 	$.getJSON("https://api.foursquare.com/v2/venues/search?client_id=PLTZT1HHN0Q20XAE5TRDFUPJLV3YKW4F5ZA00SJYPVTOHO5B&client_secret=RMJKT5CTWVEIFUEKRSAAF01TTABE53IA2OU4IGA4ZRPL1TBV&v=20171228&ll="+self.lat+","+self.long+"&query="+self.name, function(data) {
 		console.log(data);
-		if(data.response.venues.length > 0) {
-			if(data.response.venues[0].url) {
+			if(data.response.venues[0].url.length >= 1) {
 			self.url = data.response.venues[0].url;
 		}
 
-		if(data.response.venues[0].contact.formattedPhone) {
+		if(data.response.venues[0].contact.formattedPhone.length >= 1) {
 			self.phone = data.response.venues[0].contact.formattedPhone;
-		}
 		}
 		
 	});
